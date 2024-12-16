81e689d542bcad95100719cd6e901309d27fffc3
Robyn Marowitz <robyn.marowitz@colorado.edu>
2024-12-16 16:42:14 -0700

    install bitnami

--- scripts/install-argo.sh
+++ scripts/install-argo.sh
@@ -5,6 +5,7 @@ set -e
 THIS_DIR="$( cd "$(dirname "$0")"; pwd -P )"
 
 helm repo add minio https://charts.min.io/
+helm repo add bitnami https://charts.bitnami.com/bitnami
 helm dependency build helm/
 # `qgnet-argo` is the "release name".
 helm install qgnet-argo "$THIS_DIR/../helm" -n argo-helm
