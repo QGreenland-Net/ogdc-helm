7c8d29ed770a53ece767ab1688fc750c9a42561c
Trey Stafford <trey.stafford@colorado.edu>
2024-12-18 11:38:49 -0700

    local dev mount point for OGDC PV
    Now the PV can get computed dynamically in dev. By default, the PV will be
    created with a hostpath that's in the root of this project. The user can
    override that behavior through the use of the `OGDC_PV_HOST_PATH` envvar.

--- scripts/install-ogdc.sh
+++ scripts/install-ogdc.sh
@@ -2,10 +2,28 @@
 
 set -e
 
+ENV="$1"
+
+if [ "$#" -ne 1 ]; then
+    echo "Must provide environment"
+    exit 1
+fi
+
 THIS_DIR="$( cd "$(dirname "$0")"; pwd -P )"
+
+# This is used just in dev
+# TODO: only enable this behavior in dev.
+if [ "$ENV" = "dev" ]; then
+  if [ -z "$OGDC_PV_HOST_PATH" ]; then
+    OGDC_PV_HOST_PATH=$(realpath "${THIS_DIR}/../ogdc-local-hostmount/")
+  fi
+  mkdir -p OGDC_PV_HOST_PATH
+else
+    echo "env is not dev"
+fi
 
 helm repo add minio https://charts.min.io/
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm dependency build helm/
 # `qgnet-ogdc` is the "release name".
-helm install qgnet-ogdc "$THIS_DIR/../helm" -n qgnet
+helm install --set OgdcPVHostPath="$OGDC_PV_HOST_PATH" qgnet-ogdc "$THIS_DIR/../helm" -n qgnet
