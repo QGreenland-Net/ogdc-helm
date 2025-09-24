6f60853650b99d1604c0625a1137e76dd7a9dbf4
Rushiraj Nenuji <rushirajnenuji@gmail.com>
2025-09-24 09:05:28 -0700

    remove bash scripts for trivial kubectl / helm commands
    remove bash scripts for trivial kubectl / helm commands

--- scripts/forward-ports.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-kubectl --namespace qgnet port-forward services/qgnet-ogdc-argo-workflows-server 2746:2746
--- scripts/uninstall-ogdc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-RELEASE_NAME="qgnet-ogdc"
-NAMESPACE="qgnet"
-
-helm uninstall $RELEASE_NAME -n $NAMESPACE
