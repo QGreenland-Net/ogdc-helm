e365e8521b1f65ac65d528fd815e39600b801901
Rushiraj Nenuji <rushirajnenuji@gmail.com>
2025-10-08 13:56:59 -0700

    Revert "remove install scripts"
    This reverts commit ac3411d103d964eb630589c6efbeb78831e4a0bd.

---
+++ scripts/install-ogdc.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -e
+
+ENV="${1:-dev}"
+NAMESPACE="${2:-qgnet}"
+
+if [[ "$ENV" != "dev" && "$ENV" != "stage" && "$ENV" != "prod" ]]; then
+    echo "Invalid environment: $ENV"
+    echo "Usage: $0 [dev|stage|prod] [namespace]"
+    exit 1
+fi
+
+echo "Using env=${ENV}"
+echo "Using namespace=${NAMESPACE}"
+
+THIS_DIR="$( cd "$(dirname "$0")"; pwd -P )"
+
+# This is used just in dev
+if [ "$ENV" = "dev" ]; then
+  if [ -z "$OGDC_PV_HOST_PATH" ]; then
+    OGDC_PV_HOST_PATH="${THIS_DIR}/../ogdc-local-hostmount/"
+  fi
+  mkdir -p "${OGDC_PV_HOST_PATH}"
+  OGDC_PV_HOST_PATH=$(realpath "${OGDC_PV_HOST_PATH}")
+  echo "Using OGDC_PV_HOST_PATH=${OGDC_PV_HOST_PATH}"
+  
+  if [ -z "$MINIO_PV_HOST_PATH" ]; then
+    MINIO_PV_HOST_PATH="${THIS_DIR}/../ogdc-minio-local-mount/"
+  fi
+  mkdir -p "${MINIO_PV_HOST_PATH}"
+  MINIO_PV_HOST_PATH=$(realpath "${MINIO_PV_HOST_PATH}")
+  echo "Using MINIO_PV_HOST_PATH=${MINIO_PV_HOST_PATH}"
+fi
+
+echo "=== Setting up Community Helm Repositories ==="
+# Add all required community repositories (no more Bitnami!)
+echo "Adding official Argo Workflows repository..."
+helm repo add argo https://argoproj.github.io/argo-helm
+
+echo "Adding official MinIO repository..."
+helm repo add minio https://charts.min.io/
+
+helm dependency update helm/
+helm dependency build helm/
+
+RELEASE_NAME="qgnet-ogdc"
+NAMESPACE="qgnet"
+echo "Using RELEASE_NAME=${RELEASE_NAME}"
+QGNET_WORKFLOW_PVC_NAME="${RELEASE_NAME}-workflow-pvc"
+echo "Using QGNET_WORKFLOW_PVC_NAME=${QGNET_WORKFLOW_PVC_NAME}"
+
+# `qgnet-ogdc` is the "release name".
+helm install \
+  --set env="$ENV" \
+  --set OgdcNamespace="$NAMESPACE" \
+  --set QGNetWorkflowPVCName="$QGNET_WORKFLOW_PVC_NAME" \
+  --set OgdcPVHostPath="$OGDC_PV_HOST_PATH" \
+  --set MinioPVHostPath="$MINIO_PV_HOST_PATH" \
+  --set argo-workflows.controller.workflowNamespaces[0]="default" \
+  --set argo-workflows.controller.workflowNamespaces[1]="$NAMESPACE" \
+  "$RELEASE_NAME" "$THIS_DIR/../helm" \
+  -n "$NAMESPACE" --create-namespace
