47c94ee00a303a8c092b2ecbb28153fc98af3e03
Robyn Marowitz <103532760+rmarow@users.noreply.github.com>
2025-10-20 14:57:16 -0600

    Update install-ogdc.sh

--- scripts/install-ogdc.sh
+++ scripts/install-ogdc.sh
@@ -38,4 +38,5 @@ helm dependency build helm/
 # `qgnet-ogdc` is the "release name".
 envsubst < "$VALUES_FILE" | helm install \
   "$RELEASE_NAME" "$THIS_DIR/../helm" \
-  -n "$NAMESPACE" -f -
+  -n "$NAMESPACE" --create-namespace \
+  -f -
